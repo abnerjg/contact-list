bccbc3dc771382dd7e113fe3405e6bfd049cfa3a
Abner Jimenez <ajjg21@gmail.com>
2023-08-23 11:10:22 -0600

    cambios

---
+++ contact-list.js
@@ -0,0 +1,9 @@
+let listaContactos = ["FelipeCalvache", "CarlosPerez", "AbnerJimenez"];
+
+function agregarContacto (nuevoContacto) {
+listaContactos.push("nuevoContacto");
+}
+
+agregarContacto();
+
+console.log(listaContactos);
