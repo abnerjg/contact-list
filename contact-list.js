6b8fdb5ef69eca56bded36462bfbede5fc500e83
Abner Jimenez <ajjg21@gmail.com>
2023-08-23 13:28:47 -0600

    inicio

--- contact-list.js
+++ contact-list.js
@@ -1,1 +1,26 @@
-hola que tal
+let listaContactos = [
+{id: "1", 
+Nombre: "Abner", 
+Apellido: "Jimenez",
+ Telefono: "33924849",
+  Ubicaciones: {Ciudad: "Tocoa", Direccion: "Barrio El Centro"}}
+]
+
+function agregarContacto(nuevoContacto) {
+listaContactos.push(nuevoContacto);
+return listaContactos 
+}
+
+function borrarContacto(contactoABorrar){
+let nuevaLista = listaContactos.filter((contacto) => contacto !== contactoABorrar)
+listaContactos = nuevaLista 
+}
+
+function imprimirContactos (){
+console.log(listaContactos)
+}
+
+agregarContacto("Keren")
+imprimirContactos()
+borrarContacto("FelipeCalvache")
+imprimirContactos()
